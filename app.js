383d505c76d54dd699ffcc5b3004489528dba1dc
Dagmito <36495952+dagmito@users.noreply.github.com>
2018-04-22 17:01:39 -0300

    Commit with EJS, PUG and another features

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var app = require('./config/cfg_express')();
 
-app.get('/', function(request, response){
-    response.send('Roldanas da Pista!')
+app.listen(3000, function(){
+    console.log('Roldanas na pista');
 });
-
-app.listen(3000);
